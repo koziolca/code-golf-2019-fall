bda9b0fb30cbeb8bb10b9d191fb89a3d2f8296ba
Craig Koziolek <craig.a.koziolek@vanderbilt.edu>
2019-11-05 20:45:21 -0600

    maybe

---
+++ solutions/prob3.h
@@ -0,0 +1,16 @@
+/** 
+ * File name: prob3.h
+ * Author: Craig Koziolek
+ * VUnetid: koziolca
+ * Email: craig.a.koziolek@vanderbilt.edu
+ * Class: CS3251
+ * Date: 11/5/2019
+ * Honor statement: I attest that I understand the honor code for this class and have neither
+ * given nor received any unauthorized aid on this assignment.
+ * Description: 
+ */
+
+#ifndef CODE_GOLF_PROB3_H
+#define CODE_GOLF_PROB3_H
+
+#endif //CODE_GOLF_PROB3_H
